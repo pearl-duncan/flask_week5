4a8bd8597874833319cc99faa32fa69bab00d289
Pearl Osborne <pearlosborne@hotmail.com>
2023-09-28 21:50:43 -0500

    idk

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(45), nullable=False, unique=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
@@ -17,3 +18,21 @@ class User(db.Model):
         self.email = email
         self.password = password
 
+class Pokemon(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    base_experience = db.Column(db.String(100), nullable=False)
+    ability_name = db.Column(db.Integer, nullable=False)
+    sprite = db.Column(db.String, nullable=False)
+    attack = db.Column(db.String(100), nullable=False)
+    hp = db.Column(db.String(100), nullable=False)
+    defense = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, name, base_experience, ability_name, sprite, attack, hp, defense):
+        self.name = name
+        self.ability_name = ability_name
+        self.base_experience= base_experience
+        self.sprite = sprite
+        self.attack = attack
+        self.hp = hp
+        self.defense = defense
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 from app import app
-from flask import render_template, request
+from flask import flash, redirect, render_template, request, url_for
 from .forms import SignUpForm, LoginForm, SearchForm
-from .models import db, User
+from .models import db, User, Pokemon
 import requests as r
+from flask_login import login_user, logout_user, current_user, login_required
+
 
 
 @app.route("/")
@@ -34,49 +36,74 @@ def signup_page():
 
 @app.route("/login", methods=["GET", "POST"])
 def login_page():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if request.method == 'POST':
         if form.validate():
-                username = form.username.data
-                password = form.password.data
+            username = form.username.data
+            password = form.password.data
 
-            #look in database for user with that username
-            #if they exist, see if the passwords match
-            #if passwords match, consider them logged in
+            # Look in the database for a user with that username
+            user = User.query.filter_by(username=username).first() 
+            # if they exist, see if the password match
+            if user:
+                if user.password == password:
+            # if passwords match, consider them logged in
+                    login_user(user)
+                else:
+                    print('passwords dont match')
+            else:
+                print('that user doesnt exist')
 
-                user = User(username, password)
+        return redirect(url_for('index'))
+    return render_template('login.html', form = form)
 
-                db.session.add(user)
-                db.session.commit()
-
-    return render_template('login.html', form=form )
-
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login_page'))
 
 @app.route("/search", methods=["GET", "POST"])
 def search_page():
     form= SearchForm()
-    if request.method == 'POST':            
-            def get_pokemon(name):
-                name = form.name.data
-                url= f"https://pokeapi.co/api/v2/pokemon/{name}"
-                response = r.get(url)
-                if response.ok:
-                    data= response.json()
+    if request.method == 'POST': 
+        if Pokemon == True:
+            return render_template("ind_pokemon.html")
+        else:
+            flash('That pokemon does not exist', 'danger')
+            return redirect("search.html")
+    return render_template("search.html", form=form)
+                 
+        
+        
+        
+@app.route("/search/<pokemon_id>")
+@login_required
+def get_pokemon(pokemon_id):
+    pokemon = Pokemon.query.get(pokemon_id)
+    url= f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}"
+    response = r.get(url)
+    if response.ok:
+        data= response.json()
 
-                    name = data['species']['name']
-                    base_experience = data['base_experience']
-                    ability_name = data["abilities"][0]["ability"]["name"]
-                    sprite = data["sprites"]["front_shiny"]
-                    attack = data["stats"][1]["base_stat"]
-                    hp = data["stats"][0]["base_stat"]
-                    defense = data["stats"][2]["base_stat"]
-                print(name, base_experience, ability_name, hp, attack, sprite, defense)
+        name = data['species']['name']
+        base_experience = data['base_experience']
+        ability_name = data["abilities"][0]["ability"]["name"]
+        sprite = data["sprites"]["front_shiny"]
+        attack = data["stats"][1]["base_stat"]
+        hp = data["stats"][0]["base_stat"]
+        defense = data["stats"][2]["base_stat"]
+        #return f'(name: {name}, base experience: {base_experience}, ability name: {ability_name}, sprite: {sprite}, attack: {attack}, hp: {hp}, defense: {defense})'
+        return render_template("ind_pokemon.html", name=name, base_experience=base_experience, ability_name=ability_name, sprite=sprite, hp=hp, attack=attack, defense=defense )               
+
+          
+               
+            #print(name, base_experience, ability_name, hp, attack, sprite, defense)
+             
                 
-                #return f'(name: {name}, base experience: {base_experience}, ability name: {ability_name}, sprite: {sprite}, attack: {attack}, hp: {hp}, defense: {defense})'
     
-       
-    
-    return render_template("search.html", form=form) 
+     
 #pokemon_name=small_dictionary
 
 #small_dictionary = get_pokemon(pokemon_name)
